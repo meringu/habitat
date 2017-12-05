a4cffce020e616d30c9b7cc84052830f47d1b086
Josh Black <raskchanky@gmail.com>
2017-12-05 18:47:13 +0000

    testing uploads

--- test/builder-api/src/harts.js
+++ test/builder-api/src/harts.js
@@ -3,10 +3,22 @@ const supertest = require('supertest');
 const request = supertest('http://localhost:9636/v1');
 const fs = require('fs');
 
-var data = fs.readFileSync('../fixtures/neurosis-testapp-0.1.3-20171205003213-x86_64-linux.hart');
+var data = fs.readFileSync(__dirname + '/../fixtures/neurosis-testapp-0.1.3-20171205003213-x86_64-linux.hart');
 
 describe('Working with hart files', function() {
   describe('Uploading files', function() {
+    it('does not allow unauthenticated users to upload files', function(done) {
+      request.post('/depot/pkgs/neurosis/testapp/0.1.3/20171205003213')
+        .query({checksum: '3138777020e7bb621a510b19c2f2630deee9b34ac11f1c2a0524a44eb977e4a8'})
+        .set('Content-Length', data.length)
+        .send(data)
+        .expect(401)
+        .end(function(err, res) {
+          expect(res.text).to.be.empty;
+          done(err);
+        });
+    });
+
     it('allows authenticated users to upload files', function(done) {
       request.post('/depot/pkgs/neurosis/testapp/0.1.3/20171205003213')
         .set('Authorization', global.boboBearer)
@@ -15,11 +27,9 @@ describe('Working with hart files', function() {
         .send(data)
         .expect(201)
         .end(function(err, res) {
-          expect(res.body).to.equal('/pkgs/neurosis/testapp/0.1.3/20171205003213/download');
+          expect(res.text).to.equal('/pkgs/neurosis/testapp/0.1.3/20171205003213/download');
           done(err);
         });
     });
-    // it('does not allow unauthenticated users to upload files', function(done) {
-    // });
   });
 });
