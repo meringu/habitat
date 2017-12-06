7f0aed33a96b2f712b6e1a2760053d3fb9ddb4a7
Josh Black <raskchanky@gmail.com>
2017-12-06 00:36:16 +0000

    stubbing out some tests

--- test/builder-api/src/packages.js
+++ test/builder-api/src/packages.js
@@ -5,9 +5,9 @@ const fs = require('fs');
 
 var data = fs.readFileSync(__dirname + '/../fixtures/neurosis-testapp-0.1.3-20171205003213-x86_64-linux.hart');
 
-describe('Working with hart files', function() {
-  describe('Uploading files', function() {
-    it('does not allow unauthenticated users to upload files', function(done) {
+describe('Working with packages', function() {
+  describe('Uploading packages', function() {
+    it('does not allow unauthenticated users to upload packages', function(done) {
       request.post('/depot/pkgs/neurosis/testapp/0.1.3/20171205003213')
         .query({checksum: '3138777020e7bb621a510b19c2f2630deee9b34ac11f1c2a0524a44eb977e4a8'})
         .set('Content-Length', data.length)
@@ -19,7 +19,7 @@ describe('Working with hart files', function() {
         });
     });
 
-    it('allows authenticated users to upload files', function(done) {
+    it('allows authenticated users to upload packages', function(done) {
       request.post('/depot/pkgs/neurosis/testapp/0.1.3/20171205003213')
         .set('Authorization', global.boboBearer)
         .set('Content-Length', data.length)
@@ -31,5 +31,36 @@ describe('Working with hart files', function() {
           done(err);
         });
     });
+  });
+
+  describe('Finding packages', function() {
+    it('allows me to search for packages', function(done) {
+    });
+
+    it('lists all packages');
+
+    it('lists all unique package names');
+
+    it('lists all packages with the specified name');
+
+    it('lists all versions of the package with the specified name');
+
+    it('returns the latest release of a package with the specified name');
+
+    it('lists all packages with the specified name and version');
+
+    it('returns the latest release of a package with the spcified name and version');
+
+    it('returns the specified release');
+  });
+
+  describe('Other functions', function() {
+    it('lists all the channels a package is in');
+
+    it('downloads a package');
+
+    it('toggles the privacy setting for a package');
+
+    it('returns package stats');
   });
 });
