31d26369bca374263c0bd2f7a57f44f17d9753b0
Josh Black <raskchanky@gmail.com>
2017-12-11 23:29:28 +0000

    profile apis tested

--- test/builder-api/src/profile.js
+++ test/builder-api/src/profile.js
@@ -4,12 +4,55 @@ const request = supertest('http://localhost:9636/v1');
 
 describe('Profile API', function() {
   describe('Updating the profile', function() {
-    it('requires authentication');
-    it('only allows someone to update their own profile');
+    it('requires authentication', function(done) {
+      request.patch('/profile')
+        .type('application/json')
+        .accept('application/json')
+        .expect(401)
+        .end(function(err, res) {
+          expect(res.text).to.be.empty;
+          done(err);
+        });
+    });
+
+    it('allows someone to update their profile', function(done) {
+      request.patch('/profile')
+        .set('Authorization', global.boboBearer)
+        .type('application/json')
+        .accept('application/json')
+        .send({email: 'haha@example.com'})
+        .expect(200)
+        .end(function(err, res) {
+          // JB TODO: this is a sub-optimal UX here. we should return the
+          // updated profile.
+          expect(res.text).to.be.empty;
+          done(err);
+        });
+    });
   });
 
   describe('Retrieving a profile', function() {
-    it('requires authentication');
-    it('only allows someone to retrieve their own profile');
+    it('requires authentication', function(done) {
+      request.get('/profile')
+        .type('application/json')
+        .accept('application/json')
+        .expect(401)
+        .end(function(err, res) {
+          expect(res.text).to.be.empty;
+          done(err);
+        });
+    });
+
+    it('allows someone to retrieve their profile', function(done) {
+      request.get('/profile')
+        .set('Authorization', global.boboBearer)
+        .type('application/json')
+        .accept('application/json')
+        .expect(200)
+        .end(function(err, res) {
+          expect(res.body.email).to.equal('haha@example.com');
+          done(err);
+        });
+    });
   });
 });
