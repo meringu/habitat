753d668b1dd09cecc37604647841e797b9132b6f
Josh Black <raskchanky@gmail.com>
2017-12-14 17:27:09 +0000

    more origin tests

--- test/builder-api/src/origins.js
+++ test/builder-api/src/origins.js
@@ -4,13 +4,24 @@ const request = supertest('http://localhost:9636/v1');
 
 describe('Origin API', function() {
   describe('Create neurosis origin', function() {
+    it('requires authentication', function(done) {
+      request.post('/depot/origins')
+        .send({'name': 'neurosis'})
+        .expect(401)
+        .end(function(err, res) {
+          expect(res.text).to.be.empty;
+          done(err);
+        });
+    });
+
     it('returns the created origin', function(done) {
       request.post('/depot/origins')
         .set('Authorization', global.boboBearer)
-        .send({'name': 'neurosis'})
+        .send({'name': 'neurosis', 'default_package_visibility': 'private'})
         .expect(201)
         .end(function(err, res) {
           expect(res.body.name).to.equal('neurosis');
+          expect(res.body.default_package_visibility).to.equal('private');
           global.originNeurosis = res.body;
           done(err);
         });
@@ -20,7 +31,6 @@ describe('Origin API', function() {
   describe('Get origin neurosis', function() {
     it('returns the origin', function(done) {
       request.get('/depot/origins/neurosis')
-        .set('Authorization', global.boboBearer)
         .expect(200)
         .end(function(err, res) {
           expect(res.body).to.deep.equal(global.originNeurosis);
@@ -44,9 +54,46 @@ describe('Origin API', function() {
   });
 
   describe('Updating origins', function() {
-    it('requires authentication');
-    it('requires that you are a member of the origin being updated');
-    it('succeeds');
-    it('reflects the changes when viewing it again');
+    it('requires authentication', function(done) {
+      request.put('/depot/origins/neurosis')
+        .send({'default_package_visibility': 'public'})
+        .expect(401)
+        .end(function(err, res) {
+          expect(res.text).to.be.empty;
+          done(err);
+        });
+    });
+
+    it('requires that you are a member of the origin being updated', function(done) {
+      request.put('/depot/origins/neurosis')
+        .set('Authorization', global.mystiqueBearer)
+        .send({'default_package_visibility': 'public'})
+        .expect(403)
+        .end(function(err, res) {
+          expect(res.text).to.be.empty;
+          done(err);
+        });
+    });
+
+    it('succeeds', function(done) {
+      request.put('/depot/origins/neurosis')
+        .set('Authorization', global.boboBearer)
+        .send({'default_package_visibility': 'public'})
+        .expect(204)
+        .end(function(err, res) {
+          expect(res.text).to.be.empty;
+          done(err);
+        });
+    });
+
+    it('reflects the changes when viewing it again', function(done) {
+      request.get('/depot/origins/neurosis')
+        .expect(200)
+        .end(function(err, res) {
+          expect(res.body.default_package_visibility).to.equal('public');
+          global.originNeurosis = res.body;
+          done(err);
+        });
+    });
   });
 });
