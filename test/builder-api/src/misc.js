4b612f3dfc95b29d63fa48661d95dc5722e5c657
Josh Black <raskchanky@gmail.com>
2017-12-16 00:31:48 +0000

    more tests and random stuff for project tools

--- test/builder-api/src/misc.js
+++ test/builder-api/src/misc.js
@@ -1,6 +1,9 @@
 const expect = require('chai').expect;
 const supertest = require('supertest');
 const request = supertest('http://localhost:9636/v1');
+const fs = require('fs');
+
+const hookPayload = fs.readFileSync(__dirname + '/../fixtures/ping-hook.json');
 
 describe('Miscellanenous API', function() {
   describe('Retrieving the API status', function() {
@@ -16,13 +19,21 @@ describe('Miscellanenous API', function() {
     });
   });
 
-  // This test suffers from the same issue outlined in projects.js
-  describe('Authenticating with GitHub', function() {
-    it('succeeds');
-  });
-
+  // We're going to simulate receiving a GH ping hook
   describe('Receiving a GitHub webhook', function() {
-    it('succeeds');
+    it('succeeds', function(done) {
+      request.post('/notify')
+        .type('application/json')
+        .accept('application/json')
+        .set('X-Github-Event', 'ping')
+        .set('X-Hub-Signature', 'sha1=6e30dd2c021bdb935f98a827a3d31a2fbdab69d6')
+        .send(hookPayload)
+        .expect(200)
+        .end(function(err, res) {
+          expect(res.text).to.be.empty;
+          done(err);
+        });
+    });
   });
 
   // This isn't the greatest test because our testapp doesn't have any
--- test/builder-api/src/projects.js
+++ test/builder-api/src/projects.js
@@ -2,11 +2,15 @@ const expect = require('chai').expect;
 const supertest = require('supertest');
 const request = supertest('http://localhost:9636/v1');
 
-// These APIs are currently too difficult to test, given the need to obtain
-// installation and repo IDs from GH. Those can only be obtained via the flow
-// described here
-// https://developer.github.com/apps/building-github-apps/identifying-and-authorizing-users-for-github-apps/
-// which expects users in a browser, not a server-side API.
+// These values correspond to the private-test repo in the habitat-sh org
+const installationId = 56940;
+const repoId = 105209810;
+const projectCreatePayload = {
+  origin: 'neurosis',
+  plan_path: 'plan.sh',
+  installation_id: installationId,
+  repo_id: repoId
+};
 
 describe('Projects API', function() {
   describe('Creating a project', function() {
@@ -14,12 +18,7 @@ describe('Projects API', function() {
       request.post('/projects')
         .type('application/json')
         .accept('application/json')
-        .send({
-          origin: 'neurosis',
-          plan_path: 'plan.sh',
-          installation_id: 0,     // this value and the one below it need to change to valid values
-          repo_id: 0
-        })
+        .send(projectCreatePayload)
         .expect(401)
         .end(function(err, res) {
           expect(res.text).to.be.empty;
@@ -27,9 +26,59 @@ describe('Projects API', function() {
         });
     });
 
-    it('requires membership in the origin that the project refers to');
+    it('requires membership in the origin that the project refers to', function(done) {
+      request.post('/projects')
+        .type('application/json')
+        .accept('application/json')
+        .set('Authorization', global.mystiqueBearer)
+        .send(projectCreatePayload)
+        .expect(403)
+        .end(function(err, res) {
+          expect(res.text).to.be.empty;
+          done(err);
+        });
+    });
 
-    it('succeeds');
+    it('requires a properly formatted payload', function(done) {
+      request.post('/projects')
+        .type('application/json')
+        .accept('application/json')
+        .set('Authorization', global.boboBearer)
+        .send({
+          haha: 'lulz'
+        })
+        .expect(422)
+        .end(function(err, res) {
+          expect(res.text).to.be.empty;
+          done(err);
+        });
+    });
+
+    it('succeeds', function(done) {
+      request.post('/projects')
+        .type('application/json')
+        .accept('application/json')
+        .set('Authorization', global.boboBearer)
+        .send(projectCreatePayload)
+        .expect(201)
+        .end(function(err, res) {
+          // console.log('-------------------------------');
+          // console.log(res.text);
+          // console.log('-------------------------------');
+          expect(res.body.id).to.not.be.empty;
+          expect(res.body.origin_id).to.equal(global.originNeurosis.id.toString());
+          expect(res.body.origin_name).to.equal('neurosis');
+          expect(res.body.package_name).to.equal('redis');
+          expect(res.body.name).to.equal('neurosis/redis');
+          expect(res.body.plan_path).to.equal('plan.sh');
+          expect(res.body.owner_id).to.equal(global.sessionBobo.id);
+          expect(res.body.vcs_type).to.equal('git');
+          expect(res.body.vcs_data).to.equal('https://github.com/habitat-sh/private-test.git')
+          expect(res.body.vcs_installation_id).to.equal(installationId.toString());
+          expect(res.body.visibility).to.equal('public');
+          done(err);
+        });
+    });
   });
 
   describe('Retrieving a project', function() {
